233afab42da680627e80fe06b5a0e3b820333245
zachglassman <zach.glassman@gmail.com>
2017-10-16 07:57:05 -0700

    new accept in submit fetch

--- components/profile-form.js
+++ components/profile-form.js
@@ -117,7 +117,8 @@ class ProfileForm extends React.Component {
             fetch("/submit", {
                 method: "POST",
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
                 },
                 body: JSON.stringify(this.state)
             }).then((res) => {
